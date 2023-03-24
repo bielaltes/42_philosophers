f96601f49b0d0ceff8a735e74636d8e4efb70348
Biel Altes Grifoll <baltes-g@cbr15s1.42barcelona.com>
2023-03-24 14:17:31 +0100

    first

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baltes-g <baltes-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 11:38:08 by baltes-g          #+#    #+#             */
+/*   Updated: 2023/03/24 11:46:54 by baltes-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <unistd.h> 
+# include <pthread.h>
+
+#endif
